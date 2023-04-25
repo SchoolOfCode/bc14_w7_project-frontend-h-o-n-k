3f993f0e314758b5d3a374bf15d56bec8d30ea64
trisrosco <trisrosco@gmail.com>
2023-04-25 14:45:01 +0100

    The category selector works!

---
+++ honkapp/src/QuestionData.js
@@ -0,0 +1,20 @@
+export const questionDataCSS = {
+  1: {
+    image: "https://i.imgur.com/AAlLkvi.png",
+    question: "Test1",
+    score: 1,
+    difficulty: 1,
+    answer: "Test",
+  },
+};
+
+export const questionDataJS = {
+  1: {
+    image:
+      "https://i.pinimg.com/originals/4c/a3/1e/4ca31e5343e89bc550044caec29543e6.jpg",
+    question: "Test2",
+    score: 1,
+    difficulty: 1,
+    answer: "Test",
+  },
+};
--- honkapp/src/components/App/App.js
+++ honkapp/src/components/App/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import React, { useState } from "react";
+import { questionDataJS } from "../../QuestionData";
+import { questionDataCSS } from "../../QuestionData";
 
 function App() {
-  return <div className="App">Test</div>;
+  const [category, setCategory] = useState(questionDataJS[1].image);
+
+  function handleCategoryChange(event) {
+    const value = event.target.value;
+    if (value === "CSS") {
+      setCategory(questionDataCSS[1].image);
+    } else if (value === "JS") {
+      setCategory(questionDataJS[1].image);
+    }
+  }
+
+  return (
+    <>
+      <select name="difficulty" id="difficulty" onChange={handleCategoryChange}>
+        <option value="CSS">CSS</option>
+        <option value="JS">JS</option>
+      </select>
+      <img src={category} alt="test" />
+      <div className="App">Test</div>
+    </>
+  );
 }
 
 export default App;
