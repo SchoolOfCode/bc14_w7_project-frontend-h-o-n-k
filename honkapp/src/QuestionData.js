fcc3f262bc6976ee6f24c7ca236e7d2982313f30
lastcastleofbowser <123087687+lastcastleofbowser@users.noreply.github.com>
2023-04-26 11:44:29 +0100

    Js and CSS questions created

--- honkapp/src/QuestionData.js
+++ honkapp/src/QuestionData.js
@@ -1,73 +1,130 @@
 export const questionDataCSS = {
   0: {
-    image: "https://i.imgur.com/AAlLkvi.png",
-    question: "TestCSS0",
+    image: "",
+    question: "How do you select a class in CSS?",
     score: 1,
     difficulty: 1,
-    answer: "Test",
+    choice1: "#",
+    choice2: ",",
+    choice3: ".",
+    choice4: "@",
+    answer: ".",
+    
   },
   1: {
-    image: "https://i.imgur.com/AAlLkvi.png",
-    question: "what does CSS stand for",
+    image: "",
+    question: "What does CSS stand for?",
     score: 1,
     difficulty: 1,
-    choice1: "Cascading Style sheets",
-    choice2: "Cascading Style sheeps",
-    choice3: "Cascading Style ships",
-    choice4: "Cascading Style snakes",
-    answer: "Cascading Style sheets",
+    choice1: "Cascading Style Sheets",
+    choice2: "Cute Style Sheets",
+    choice3: "Centered Style System",
+    choice4: "Cascading Style Service",
+    answer: "Cascading Style Sheets",
   },
 
   2: {
-    image: "https://i.imgur.com/AAlLkvi.png",
-    question: "TestCSS2",
+    image: "https://i.imgur.com/xrEr66g.png",
+    question: "Fill in the blank below to make this container into a flexbox",
     score: 1,
     difficulty: 1,
-    answer: "Test",
+    choice1: "grid",
+    choice2: "inline-box",
+    choice3: "justify-items",
+    choice4: "flex",
+    answer: "flex",
   },
+  
 
   3: {
-    image: "https://i.imgur.com/AAlLkvi.png",
-    question: "TestCSS3",
+    image: "https://i.imgur.com/iJWzY8Q.png",
+    question: "What property would you use to make this button have rounded edges?",
     score: 1,
     difficulty: 1,
-    answer: "Test",
+    choice1: "rounded-edges",
+    choice2: "border-radius",
+    choice3: "border-edge",
+    choice4: "box-round",
+    answer: "border-radius",
   },
+  
+  4: {
+    image: "",
+    question: "Which of the following creates a margin around the element of 10px at the top, 20px at the sides and 2px at the bottom?",
+    score: 1,
+    difficulty: 1,
+    choice1: "margin: 20px 10px 2px 2px",
+    choice2: "margin: 10px 20px 2px",
+    choice3: "margin: 10 20 2",
+    choice4: "margin: 2px 10px 20px",
+    answer: "margin: 10px 20px 2px",
+  },
+
 };
 
 export const questionDataJS = {
   0: {
-    image: "https://i.imgur.com/AAlLkvi.png",
-    question: "TestJS0",
+    image: "",
+    question: "What type of variable is a 'const'?",
     score: 1,
     difficulty: 1,
-    answer: "Test",
+    choice1: "Block-scoped",
+    choice2: "Global-scoped",
+    choice3: "Local-scoped",
+    choice4: "Function-scoped",
+    answer: "Block-scoped",
   },
 
   1: {
-    image:
-      "https://i.pinimg.com/originals/4c/a3/1e/4ca31e5343e89bc550044caec29543e6.jpg",
-    question: "TestJS1",
+    image:"https://i.imgur.com/5RDpqMc.png",
+    question: "What missing word should go in the blank space below?",
     score: 1,
     difficulty: 1,
-    answer: "Test",
+    choice1: "let",
+    choice2: "print",
+    choice3: "sum",
+    choice4: "return",
+    answer: "return",
   },
 
   2: {
-    image:
-      "https://i.pinimg.com/originals/4c/a3/1e/4ca31e5343e89bc550044caec29543e6.jpg",
-    question: "TestJS2",
+    image:"https://i.imgur.com/dkoR2BE.png",
+    question: "Read the function below. What is the value of 'x'?",
     score: 1,
     difficulty: 1,
-    answer: "Test",
+    choice1: "0.5",
+    choice2: "1",
+    choice3: "2",
+    choice4: "0",
+    answer: "1",
   },
 
   3: {
-    image:
-      "https://i.pinimg.com/originals/4c/a3/1e/4ca31e5343e89bc550044caec29543e6.jpg",
-    question: "TestJS3",
+    image: "https://i.imgur.com/h5acI50.png",
+    question: "How would you select the key with the value 'Buenos Aires' in this nested object?",
     score: 1,
     difficulty: 1,
-    answer: "Test",
+    choice1: "capital[1]",
+    choice2: "theWorld.country[0].capital",
+    choice3: "continent.country['Buenos Aires']",
+    choice4: "theWorld.continent.country[0].capital",
+    answer: "theWorld.country[0].capital",
+  },
+  
+
+  4: {
+    image:"https://i.imgur.com/1ujS9k4.png",
+    question: "What word is missing from the code below?",
+    score: 1,
+    difficulty: 1,
+    choice1: "Sum",
+    choice2: "Maths",
+    choice3: "Calculate",
+    choice4: "Math",
+    answer: "Math",
   },
 };
+
+
+
+  
