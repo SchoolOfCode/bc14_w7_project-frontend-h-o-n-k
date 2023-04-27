c989105346edd7b154ca289c823dfcda9fa6fefc
trisrosco <trisrosco@gmail.com>
2023-04-27 11:58:35 +0100

    tests sorted m8 sick sick sick

--- honkapp/src/components/App/App.test.js
+++ honkapp/src/components/App/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test('Does the "Select a category" text appear?', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Select a category!/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("Click on the dropdown and select JS. Do buttons appear?", async () => {
+  render(<App />);
+  let dropdown = await screen.findByText(/Select a category!/i);
+  userEvent.click(dropdown);
+  let js = await screen.findByText(/JS/i);
+  userEvent.click(js);
+  const button = screen.getByRole("button");
+  expect(button).toBeInTheDocument();
 });
